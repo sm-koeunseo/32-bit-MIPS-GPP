f03feb27308517ac35f99000209cca4c262c171b
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-06-23 04:50:39 +0900

    #3 Delete BSR

--- src/ALU.v
+++ src/ALU.v
@@ -7,27 +7,20 @@ module ALU(op_code, operand1, operand2, result, enable);
     output reg [(`D_WIDTH-1):0] result;
     input enable;
 
-    reg en;
-    reg [2:0] BS_AMT;
-    wire [(`D_WIDTH-1):0] BS_RESULT;
-    BSR bsr(en, BS_AMT, operand1, BS_RESULT);
-
     always @* begin
         if (enable) begin
-            en <= 1'b0;
             case(op_code)
-                0: result <= operand1 + operand2;        // add, addi
-                1: result <= operand1 - operand2;        // sub
-                2: result <= operand1 * operand2;        // mult
-                3: begin                                // div
-                    //result = operand1 / operand2;
-                    //$display("%h, %b, %d", operand2, operand2, operand2[2:0]);
-                    en <= 1'b1;
-                    BS_AMT <= operand2[2:0];
-                    result <= BS_RESULT;
+                0: result <= operand1 + operand2;   // add, addi
+                1: result <= operand1 - operand2;   // sub
+                2: result <= operand1 * operand2;   // mult
+                3: begin                            // div
+                    case (operand2[2:0])
+                        3'b100: result <= operand1 >> 2;
+                        3'b010: result <= operand1 >> 1;
+                    endcase
                 end
-                4: result <= operand1 << operand2;       // shift left
-                5: result <= operand1 >> operand2;       // shift right
+                4: result <= operand1 << operand2;  // sll
+                5: result <= operand1 >> operand2;  // srl
             endcase
         end
     end
